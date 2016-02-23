5176b7c0c23b4828d7fbd1c6b15bf977baea914f
bhargav uppalapati <bhargav.uppalapati1@marist.edu>
2016-02-22 23:02:39 -0500

    Lab 2 AES Round Key Generation
    changes made to driver. java and aescipher.java

--- Lab 2 AES Round Key Generation/aescipher.java
+++ Lab 2 AES Round Key Generation/aescipher.java
@@ -1,3 +1,23 @@
 public class aescipher {
+	private static String[][] inputKeyMatrix = new String[4][4];
+    public static String[][] generatedKeyMatrix = new String[4][44];
 	
+    public void inputtingStringIntoKMatrix(String key)
+    {   try{
+        int i =0;
+        for (int column = 0; column < 4; column++) {            
+        for (int row = 0; row < 4; row = row + 1) {
+        	inputKeyMatrix[row][column] = key.substring(i,i+2);
+                i=i+2;
+            }
+        }
+        generatingWHexMatrix();
+    }
+    catch(Exception ex){
+    	System.err.println("Error occured in generation of input 4x4 matrix ");
+    }
+    }
+    public static void generatingWHexMatrix(){
+    
+    }
 }
--- Lab 2 AES Round Key Generation/driver.java
+++ Lab 2 AES Round Key Generation/driver.java
@@ -1,3 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 public class driver {
-	
+	public static void main(String[] args) throws IOException {       
+        try{
+        	BufferedReader standardInput = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Enter Key");
+            String keyValue = standardInput.readLine();
+            aescipher cipher = new aescipher();
+            cipher.inputtingStringIntoKMatrix(keyValue);
+        }catch(IOException excep){
+            System.err.println("Invalid Format in ");
+        }
+    }
 }
